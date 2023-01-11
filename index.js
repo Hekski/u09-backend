3d1eaf56ec0137111c4379161196128d42b38fac
LesHank <henrik.berglund@gmail.com>
2023-01-11 22:41:48 +0100

    deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const routes = require('./routes');
@@ -12,23 +12,26 @@ const routes = require('./routes');
 // DB connection
 const mongo_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
 mongoose.connect(mongo_URI, {
-  dbName: 'u09-music-streaming-app',
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+   dbName: 'u09-music-streaming-app',
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
 });
 
 // MIDDLEWARES - Parsing & Sanitize
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+/* app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); */
 app.use(cookieParser());
 app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:3000', 'https://taupe-llama-7bcb9e.netlify.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  })
+   cors({
+      credentials: true,
+      origin: [
+         'http://localhost:3000',
+         'https://taupe-llama-7bcb9e.netlify.app',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   })
 );
 app.use(xss());
 app.use(mongoSanitize());
@@ -39,5 +42,5 @@ app.use('/api', routes);
 // PORT
 const port = process.env.DB_PORT || 8001;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+   console.log(`Server running on port ${port}`);
 });
