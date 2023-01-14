4405e05166afccd3a91ac7a0fdeb7e48e8f52f76
LesHank <henrik.berglund@gmail.com>
2023-01-14 14:55:35 +0100

    minor tweak

--- models/user.js
+++ models/user.js
@@ -1,77 +1,76 @@
 const mongoose = require('mongoose');
-/* const validator = require('validator'); */
 const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    maxLength: 50,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user',
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  likedSongs: {
-    type: [],
-    default: [],
-  },
-  playlists: {
-    type: [],
-    default: [],
-  },
+   name: {
+      type: String,
+      maxLength: 50,
+      trim: true,
+   },
+   email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+   },
+   password: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   isAdmin: {
+      type: Boolean,
+      default: false,
+   },
+   role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+   },
+   date: {
+      type: Date,
+      default: Date.now,
+   },
+   likedSongs: {
+      type: [],
+      default: [],
+   },
+   playlists: {
+      type: [],
+      default: [],
+   },
 });
 
 userSchema.pre('save', async function (next) {
-  let user = this;
+   let user = this;
 
-  if (user.isModified('password')) {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(user.password, salt);
-    user.password = hash;
-  }
-  next();
+   if (user.isModified('password')) {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(user.password, salt);
+      user.password = hash;
+   }
+   next();
 });
 
 userSchema.statics.emailTaken = async function (email) {
-  const user = await this.findOne({ email });
-  return !!user;
+   const user = await this.findOne({ email });
+   return !!user;
 };
 
 userSchema.methods.generateAuthToken = function () {
-  let user = this;
-  const userObj = { sub: user._id.toHexString(), email: user.email };
-  const token = jwt.sign(userObj, process.env.DB_SECRET, { expiresIn: '1d' });
-  return token;
+   let user = this;
+   const userObj = { sub: user._id.toHexString(), email: user.email };
+   const token = jwt.sign(userObj, process.env.DB_SECRET, { expiresIn: '1d' });
+   return token;
 };
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
-  // candidate password = un-hashed password
-  const user = this;
-  const match = await bcrypt.compare(candidatePassword, user.password);
-  return match;
+   // candidate password = un-hashed password
+   const user = this;
+   const match = await bcrypt.compare(candidatePassword, user.password);
+   return match;
 };
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = { User };
