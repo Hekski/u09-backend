c04494ebc73c3fc4550acdc417bd844563940fd0
LesHank <henrik.berglund@gmail.com>
2023-01-14 14:55:24 +0100

    better user handling

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,29 @@
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
+const User = require('../models/user.js');
 
 const auth = asyncHandler(async (req, res, next) => {
-  try {
-    const token = req.cookies.jwttoken;
-    
-    if (!token) {
-      return next('Please login to access the data');
-    }
-    
-    const decoded = jwt.verify(token, process.env.DB_SECRET);
-    
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.clearCookie('jwttoken');
-    return res.status(400).json({
-      success: false,
-      message: 'Please log in',
-    });
-  }
+   try {
+      const token = req.cookies.jwttoken;
+
+      if (!token) {
+         return next('Please login to access the data');
+      }
+
+      const decoded = jwt.verify(token, process.env.DB_SECRET);
+
+      req.user = await User.findById(decoded.id).select('-password');
+      // req.user = decoded;
+
+      console.log('req.header: ', req.header);
+      next();
+   } catch (error) {
+      // res.clearCookie('jwttoken');
+      return res.status(400).json({
+         success: false,
+         message: 'Please log in',
+      });
+   }
 });
 
 module.exports = auth;
