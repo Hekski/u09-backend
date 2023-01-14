28a9f4a85a76a31026630b62b78bfc0b36367597
LesHank <henrik.berglund@gmail.com>
2023-01-14 23:52:25 +0100

    auth protection fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,26 +3,27 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/user.js');
 
 const auth = asyncHandler(async (req, res, next) => {
-   try {
-      const token = req.cookies.jwttoken;
-
+   let token;
+   if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+   ) {
+      try {
+         token = req.headers.authorization.split(' ')[1];
+         console.log(token);
+         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+         console.log('decoded', decoded);
+         req.user = decoded;
+         next();
+      } catch (error) {
+         return res.status(400).json({
+            success: false,
+            message: 'Please log in',
+         });
+      }
       if (!token) {
          return next('Please login to access the data');
       }
-
-      const decoded = jwt.verify(token, process.env.DB_SECRET);
-
-      req.user = await User.findById(decoded.id).select('-password');
-      // req.user = decoded;
-
-      console.log('req.header: ', req.header);
-      next();
-   } catch (error) {
-      // res.clearCookie('jwttoken');
-      return res.status(400).json({
-         success: false,
-         message: 'Please log in',
-      });
    }
 });
 
