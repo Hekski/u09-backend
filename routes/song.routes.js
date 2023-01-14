98609ea957906ab06e8b2739fa136684efdc0335
LesHank <henrik.berglund@gmail.com>
2023-01-14 14:54:40 +0100

    removed validateObjectId

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const songController = require('../controllers/song.controller');
 const router = express.Router();
-const validateObjectId = require('../middleware/validateObjectId');
+// const validateObjectId = require('../middleware/validateObjectId');
 
 // Middleware
 const auth = require('../middleware/auth');
 
 // Routes
 router.get('/like/:id', auth, songController.getLikes);
-router.post('/like/:id', validateObjectId, auth, songController.postLike);
-router.delete('/like/:id', validateObjectId, auth, songController.deleteLike);
+router.post('/like/:id', auth, songController.postLike);
+router.delete('/like/:id', auth, songController.deleteLike);
 
 module.exports = router;
